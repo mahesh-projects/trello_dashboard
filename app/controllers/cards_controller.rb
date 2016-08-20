99b203108f083ebcc265643cef7d3a31302b583a
Mahesh Shivamallappa <mahesh.shivamallappa@rea-group.com>
2016-08-20 16:23:34 +1000

    Refactor model to parse JSON. Setting up for generating CSV

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -3,6 +3,8 @@ require 'json'
 class CardsController < ApplicationController
   def index
   	@list_id = '570f165bd6b5fba021c69de4'
-  	@cards = Card.for @list_id
+  	#@cards = Card.for @list_id
+
+  	@cards = Card.to_csv @list_id
   end
 end
