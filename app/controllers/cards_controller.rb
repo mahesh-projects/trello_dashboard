e8913f3a454f4f18ef92b2509e0b26de42db5eea
Mahesh Shivamallappa <mahesh.shivamallappa@rea-group.com>
2016-08-21 20:29:16 +1000

    Cleaning up comments

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -5,6 +5,6 @@ class CardsController < ApplicationController
   	@list_id = '570f165bd6b5fba021c69de4'
   	#@cards = Card.for @list_id
 
-  	@cards = Card.to_csv @list_id
+  	@cards = Card.to_csv @list_id 
   end
 end
