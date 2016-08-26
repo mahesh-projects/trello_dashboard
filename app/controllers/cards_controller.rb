0450c24fa560606510f2fd4240be3295df389f3d
Mahesh Shivamallappa <mahesh.shivamallappa@rea-group.com>
2016-08-26 14:12:33 +1000

    Refactor the card model to make it easier to read and reuse

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -5,7 +5,7 @@ class CardsController < ApplicationController
   	@list_id = '570f165bd6b5fba021c69de4'
   	#@cards = Card.for @list_id
 
-  	@cards = Card.to_csv @list_id 
+  	@cards = Card.get_cards @list_id 
   	respond_to do |format|
 	  	format.html # show.html.erb
 	  	format.json { render json: @cards }
--- app/models/card.rb
+++ app/models/card.rb
@@ -4,94 +4,89 @@ require 'date'
 
 class Card < ActiveRecord::Base
 	include HTTParty
-
+	#Setup to call Trello
 	key_value = ENV["TRELLO_API_KEY"]
 	token_value	= ENV["TRELLO_API_SECRET"]
 	base_uri  "https://api.trello.com/1/lists"
 	
-	#base_uri "https://api.trello.com/1/boards/570f15504dce0625826fbae7"
 	default_params key: key_value, token: token_value 
 	format :json
 
-	def self.for list_id
-		
-		get("/#{list_id}/cards")
-	end
-
-	def self.to_csv list_id
-
-		@cards = call_trello(list_id)
-		
-		@cards
-	end
-
-	private
-
-	def self.call_trello list_id
-
-		#create an empty array. this will be used to store the hashes cards
-		#each hash would contain name, days, category E.g.: {name: "make popcorn [2]", days: 2, labels: ["Popcorn", "Fun"]}
-		@cards = []
-		
+	#Method to get the cards - either from Trello OR from DB
+	def self.get_cards list_id
 
 		if Card.count == 0 || Date.today > Card.first["updated_at"]
 
-			puts "call trello"
-			
-			Card.destroy_all
-			#store the response in @results
-			@results = get("/#{list_id}/cards")
-			
-			#loop through the results to extract card_name, days and category
-			@results.each do |result|
-				#Extract card name
-				name = result["name"]
-
-				#Extract the number of days from the end of the name string
-				count = result["name"].scan(/\[(.*)\]/)[0]
-				days = count[0].to_i unless count.nil?
-
-				#Extract the shortUrl for each card 
-				short_url = result["shortUrl"]
-
-				#Initialize an empty array to store the labels
-				#Loop through the labels and push them into the array
-				labels = []
-
-				result["labels"].each do |label|
-					labels << label["name"]
-				end
-
-				#Use the first label to classify the card
-				card_type = classify_card_type(labels[0])
-
-				#Push the hash into @cards array
-				@cards << {"name" => name, "days" => days, "labels" => labels[0], "url" => short_url, "card_type" => card_type}
-				
-
-				#Save the card into the Cards table
-				#Convert the labels array to comma separated string
-				store_to_db({"name" => name, "days" => days, "labels" => labels.to_csv, "url" => short_url, "card_type" => card_type})
-
-			end
-			#puts "call api"
-			
+			call_trello(list_id)
 
 		else
 
-			puts "retrieve from table"
-			Card.all.each do |card|
-				@cards << {"name" => card["card_name"], "days" => card["number_of_days"], "labels" => card["labels"].parse_csv[0], "url" => card["short_url"], "card_type" => card["card_type"]}
-			end
-			
-		end
+			get_cards_from_db
 
-		
-		@cards
-		
+		end		
 	end
 
+	private
+	#Call Trello to get a refresh the records in Cards table
+	def self.call_trello list_id
+		#create an empty array. this will be used to store the hashes cards
+		#each hash would contain name, days, category E.g.: {name: "make popcorn [2]", days: 2, labels: ["Popcorn", "Fun"]}
+		@cards = []
+		puts "call trello"
+			
+		Card.destroy_all
+		#store the response in @results
+		@results = get("/#{list_id}/cards")
+		
+		#loop through the results to extract card_name, days and category
+		@results.each do |result|
+			#Extract card name
+			name = result["name"]
 
+			#Extract the number of days from the end of the name string
+			count = result["name"].scan(/\[(.*)\]/)[0]
+			days = count[0].to_i unless count.nil?
+
+			#Extract the shortUrl for each card 
+			short_url = result["shortUrl"]
+
+			#Initialize an empty array to store the labels
+			#Loop through the labels and push them into the array
+			labels = []
+
+			result["labels"].each do |label|
+				labels << label["name"]
+			end
+
+			#Use the first label to classify the card
+			card_type = classify_card_type(labels[0])
+
+			#Push the hash into @cards array
+			@cards << {"name" => name, "days" => days, "labels" => labels[0], "url" => short_url, "card_type" => card_type}
+			
+
+			#Save the card into the Cards table
+			#Convert the labels array to comma separated string
+			store_to_db({"name" => name, "days" => days, "labels" => labels.to_csv, "url" => short_url, "card_type" => card_type})
+
+		end
+		@cards
+	end
+
+	#Get cards from DB
+	def self.get_cards_from_db 
+		#create an empty array. this will be used to store the hashes cards
+		#each hash would contain name, days, category E.g.: {name: "make popcorn [2]", days: 2, labels: ["Popcorn", "Fun"]}
+		@cards = []
+		puts "retrieve from table"
+		Card.all.each do |card|
+			@cards << {"name" => card["card_name"], "days" => card["number_of_days"], "labels" => card["labels"].parse_csv[0], "url" => card["short_url"], "card_type" => card["card_type"]}
+		end
+		@cards
+
+	end
+
+	#Store cards into the DB Table
 	def self.store_to_db card	
 
 		newCard = Card.new
@@ -109,7 +104,7 @@ class Card < ActiveRecord::Base
 
 	end
 
-
+	#Classify the cards as Feature, Custodianship etc before saving them to the table
 	def self.classify_card_type first_label
 
 		card_type = ''
